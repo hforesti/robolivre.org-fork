5327680fc5166691cf30785e44f4b5f8e7639e4a
Max Guenes <maxguenes16@gmail.com>
2012-03-27 16:21:13 -0300

    Arquivos web rodrigo

--- robolivre.org/trunk/web/assets/js/main-robolivre.js
+++ robolivre.org/trunk/web/assets/js/main-robolivre.js
@@ -1,4 +1,54 @@
+//float do grid de thumbs
+$(window).load(function(){
+	var $container = $('#explore .tab-pane ul');
+	$container.imagesLoaded(function(){
+		$container.masonry({
+			itemSelector : 'li',
+			isFitWidth: true,
+			gutterWidth: true,
+			isAnimated: true,
+			animationOptions: {
+				duration: 400
+			},
+			cornerStampSelector: '.corner-stamp'
+		});
+	});
+});
+
 !function ($) {
+
+//sidebar border (apenas para desktop)
+
+// if ( $("#sidebar") ) {
+// 	var sh = $("#sidebar").height();
+// 	var mmh = $(".container .span7").height();
+// 	var mh = $(".container .span10").height();
+// 	if( mmh > sh ) {
+// 		$("#sidebar").height(mmh);
+// 	}
+// 	if( mh > sh ) {
+// 		$("#sidebar").height(mh);
+// 	}
+// }
+
+
+//oculta parte da descricao do conteudo
+var ch = $('#main-entry').height();
+$('#main-entry').css({
+	height: '110px',
+	overflow: 'hidden'
+});
+$('#shink-content .btn').click(function(){
+	$('#main-entry').animate({
+		height: ch
+	}, 1800, function(){
+		$('#main-entry').css({
+			height: 'auto'
+		})
+	});
+	$(this).parent().fadeOut(700);
+	return false;
+})
 
 //oculta topbar do safari mobile
 window.top.scrollTo(0, 1);
@@ -30,6 +80,38 @@ $('#explore .search-query').blur(function() {
 	}, 250);
 });
 
+//foca no item na lista
+$('.list-mgmt .btn-group').children().hover(function() {
+	$(this).closest('.row').siblings().stop().fadeTo(500,0.45);
+}, function() {
+	$(this).closest('.row').siblings().stop().fadeTo(500,1);
+});
+$('table td .btn-group').children().hover(function() {
+	$(this).closest('tr').siblings().stop().fadeTo(500,0.45);
+}, function() {
+	$(this).closest('tr').siblings().stop().fadeTo(500,1);
+});
+
+//pega destinatario do modal de mensagem privada
+$('.send-msg').click(function(){
+	var n = $(this).closest('.row').find("h3 a").text();
+	var u = $(this).closest('.row').find(".photo img").attr("src");
+		//quando iniciar a mostrar o modal, muda nome
+		$('#modalSendMsg').on('show', function () {
+			$("#fromField .uneditable-input").text(n);
+			$("#fromField .add-on img").attr("src",u);
+		})
+
+	});
+
+//foca input from no modal de convite
+$('#modalInvite').on('show', function() {
+	$('#fromInvite').focus()
+})
+$('.bt-reply a').click(function() {
+	$('#reply-topic').focus();
+})
+
 //animacao respira Explore
 function toFade() { //reduces the opacity
 	$("#explore h2").animate({opacity: '.5'}, 1600, function() {
@@ -47,22 +129,22 @@ toFade();
 if ( $("#stream .video-embed") ) {
 	$("#stream .share-content .shared-item").click(function() {
 		$(this).fadeOut(function(){
-				url = $(this).parent().find(".video-embed iframe").attr("src");
-				$(this).parent().find(".video-embed iframe").attr({src:url+"?autoplay=1"});
-				$(this).parent().find(".video-embed").fadeIn();
+			url = $(this).parent().find(".video-embed iframe").attr("src");
+			$(this).parent().find(".video-embed iframe").attr({src:url+"?autoplay=1"});
+			$(this).parent().find(".video-embed").fadeIn();
 		});
 		return false;
 	});
 }
 
 //tooltips
-$('#grid-comunidades img, #grid-conteudos img, #grid-amigos img, #grid-projetos img, .visivel-para i').tooltip();
-$('#form-status .nav a').tooltip({ placement: 'bottom' });
+$('#grid-comunidades img, #grid-conteudos img, #grid-amigos img, #grid-projetos img, .visivel-para i, #grid-eventos img').tooltip();
+$('#form-status .nav a').tooltip({placement: 'bottom'});
 
 //textarea de comentarios
 $('.textarea-comment').autoResize({
-    maxHeight: 200,
-    extraSpace: 0
+	maxHeight: 200,
+	extraSpace: 0
 });
 
 
@@ -77,26 +159,27 @@ $("#terms-textarea").focus(function(){
 // WYSIWYG Editor
 $(".wysiwyg").cleditor({
 			controls:     // controls to add to the toolbar
-                        "bold italic strikethrough | subscript superscript | " +
-                        "highlight removeformat | bullets numbering | outdent " +
-                        "indent | " +
-                        "link unlink | source",
+			"bold italic strikethrough | subscript superscript | " +
+			"highlight removeformat | bullets numbering | outdent " +
+			"indent | " +
+			"link unlink | source",
 			colors:       // colors in the color popup
-                        "FFF FCC FC9 FF9 FFC 9F9 9FF CFF CCF FCF " +
-                        "CCC F66 F96 FF6 FF3 6F9 3FF 6FF 99F F9F " +
-                        "BBB F00 F90 FC6 FF0 3F3 6CC 3CF 66C C6C ",
+			"FFF FCC FC9 FF9 FFC 9F9 9FF CFF CCF FCF " +
+			"CCC F66 F96 FF6 FF3 6F9 3FF 6FF 99F F9F " +
+			"BBB F00 F90 FC6 FF0 3F3 6CC 3CF 66C C6C ",
 			bodyStyle:    // style to assign to document body contained within the editor
-                        "margin:4px; font:13px 'Helvetica Neue', Helvetica, Arial, sans-serif; cursor:text",
+			"margin:4px; font:13px 'Helvetica Neue', Helvetica, Arial, sans-serif; cursor:text",
 			width:'99%',
-});                
+		});                
 
 /* multi upload */
 //upload img
-var uploader = new qq.FileUploader({
+
+var uploaderImg = new qq.FileUploader({
     // pass the dom node (ex. $(selector)[0] for jQuery users)
     element: document.getElementById('file-uploader-img'),
     // path to server-side upload script
-    action: '../assets/js/file-uploader/',
+    action: url_for('ajax/ajaxUlpoadImagens'),//'../uploads/',
     multiple: false,
 	
 	// validation    
@@ -110,7 +193,15 @@ var uploader = new qq.FileUploader({
             emptyError		: "'{file}' está vazio, por favor tente novamente.",
             onLeave			: "O arquivo está sendo enviado, se você sair agora o envio será cancelado."            
 	},
-	showMessage: function(message){ alert(message); }
+	showMessage: function(message){ 
+                alert(message);
+                $("#thumb").attr("src",url_for("/assets/img/rl/170.gif")); 
+                $("#imagem_selecionada").val("");
+            },
+        onComplete: function(id, fileName, responseJSON){
+            $("#thumb").attr("src",url_for("/uploads/"+ fileName));
+            $("#imagem_selecionada").val(fileName);
+        }
 });
 $("#img-preview").click(function() {
 	$("#file-uploader-img input").click();
@@ -121,7 +212,7 @@ var uploader = new qq.FileUploader({
     // pass the dom node (ex. $(selector)[0] for jQuery users)
     element: document.getElementById('file-uploader'),
     // path to server-side upload script
-    action: '../assets/js/file-uploader/',
+    action: url_for('ajax/ajaxUlpoadArquivos'),//'../uploads/',
 	// validation    
 	// ex. ['jpg', 'jpeg', 'png', 'gif'] or []
 	allowedExtensions: ['txt', 'rtf', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'odt', 'fodt', 'odp', 'fodp', 'ods', 'fods', 'odg', 'fodg'],
@@ -133,35 +224,34 @@ var uploader = new qq.FileUploader({
             emptyError		: "'{file}' está vazio, por favor tente novamente.",
             onLeave			: "Os arquivos estão sendo enviados, se você sair agora o envio será cancelado."            
 	},
-	showMessage: function(message){ alert(message); }
+	showMessage: function(message){alert(message);}
 
 });
-
 
 }(window.jQuery)
 
 function getForcaSenha(inputPassword,spanHelp) {
-    
-    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
-    var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
-    var enoughRegex = new RegExp("(?=.{6,}).*", "g");
-    var resposta = "";
-    if (inputPassword.value.length==0) {
-      resposta = 'Campo obrigatório!';
-    } else if (false == enoughRegex.test(inputPassword.value)) {
-        resposta = 'Insuficiente';
-    } else if (strongRegex.test(inputPassword.value)) {
-        resposta = 'Forte';
-    } else if (mediumRegex.test(inputPassword.value)) {
-        resposta = 'Normal';
-    } else {
-        resposta = 'Fraca';
-    }
-    
-    if(spanHelp != undefined){
-        spanHelp.innerHTML = resposta;
-    }
-    
-    return resposta;
-    
+
+	var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+	var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+	var enoughRegex = new RegExp("(?=.{6,}).*", "g");
+	var resposta = "";
+	if (inputPassword.value.length==0) {
+		resposta = 'Campo obrigatório!';
+	} else if (false == enoughRegex.test(inputPassword.value)) {
+		resposta = 'Insuficiente';
+	} else if (strongRegex.test(inputPassword.value)) {
+		resposta = 'Forte';
+	} else if (mediumRegex.test(inputPassword.value)) {
+		resposta = 'Normal';
+	} else {
+		resposta = 'Fraca';
+	}
+
+	if(spanHelp != undefined){
+		spanHelp.innerHTML = resposta;
+	}
+
+	return resposta;
+
 }
