ab22eb3f68d4e044f964455cf0c855a7f615bd47
Rodrigo Muniz <eu@rodrigomuniz.com>
2012-04-30 19:04:32 -0300

    Início das páginas institucionais finais para o EXPO

--- apps/frontend/modules/institucional/templates/apresentacoesSuccess.php
+++ apps/frontend/modules/institucional/templates/apresentacoesSuccess.php
@@ -1,38 +1,25 @@
 <div class="row">
 
-    <div class="span4">
-        <h6>Institucional</h6>
-        <ul class="nav nav-tabs nav-stacked">
-            <li class="active"><a href="sobre.shtml">Sobre a Robô Livre</a></li>
-            <li><a href="sobre.shtml">Instituições parceiras</a></li>
-            <li><a href="sobre.shtml">Clipping</a></li>
-            <li><a href="sobre.shtml">Apresentações</a></li>
-            <li><a href="sobre.shtml">Publicações científicas</a></li>
-        </ul>
+<div class="span4">
+<h6>Institucional</h6>
+    <ul class="nav nav-tabs nav-stacked">
+        <li><a href="<?php echo url_for("institucional/sobre") ?>">Sobre a Robô Livre</a></li>
+        <li><a href="<?php echo url_for("institucional/instituicoesParceiras") ?>">Instituições parceiras</a></li>
+        <li class="active"><a href="<?php echo url_for("institucional/apresentacoes") ?>">Apresentações</a></li>
+        <li><a href="<?php echo url_for("institucional/publicacoesCientificas") ?>">Publicações científicas</a></li>
+    </ul>
         <a href="<?php echo url_for("perfil/index") ?>" class="btn"><i class="icon-chevron-left icon-gray"></i> Início</a>
-    </div>
+</div>
 
 
     <div class="span8">
 
         <div class="page-header">
-            <h1>Sobre a Robô Livre</h1>
+            <h1>Apresentações</h1>
         </div>
 
-        <h2>Nossa filosofia</h2>
-        <p>
-            <strong>Modelo de página com vários tipos de conteúdo. Por exemplo, subtítulos, tabela para downloads de apresentações ou artigos publicados, grid de imagens da equipe ou para logo de parceiros. </strong></p>
+        <p>Aqui você encontra um diretório de downloads onde estarão sempre disponíveis os arquivos de apresentações feitas por nossa equipe. Confira a tabela a seguir:</p>
 
-        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna mollis ornare vel eu leo.
-        </p>
-        <h3>Tortor mauris condimentum nibh</h3>
-        <p>
-            Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur.</p>
-
-        <h4>Fusce dapibus</h4>
-        <p>
-            Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.
-        </p>
 
         <h2>Downloads</h2>
         <table class="table table-striped table-bordered">
@@ -63,76 +50,8 @@
         </table>
 
         <p>
-            Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Não encontrou uma apresentação nossa? <a href="<?php echo url_for("contato") ?>">Entre em contato conosco</a>.
         </p>
-
-        <h5>Fusce dapibus, tellus ac cursus commodo</h5>
-        <p>
-            Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        </p>
-        <h6>Fusce dapibus</h6>
-        <ul>
-            <li>Condimentum Sollicitudin</li>
-            <li>Condimentum Sollicitudin Consectetur Tristique</li>
-            <li>Condimentu Consectetur Tristique</li>
-        </ul>
-
-        <h6>Fusce dapibus</h6>
-        <ol>
-            <li>Condimentum Sollicitudin</li>
-            <li>Condimentum Sollicitudin Consectetur Tristique</li>
-            <li>Condimentu Consectetur Tristique</li>
-        </ol>
-
-        <h2>Equipe</h2>
-        <p>
-            Quer saber quem faz a Robô Livre?
-        </p>
-
-        <ul class="thumbnails">
-            <li class="span2">
-                <div class="thumbnail">
-                    <a href="perfil.shtml"><img src="/assets/img/rl/avatar-170.jpg" alt="Rodrigo Medeiros"></a>
-                    <strong>Rodrigo Medeiros</strong>
-                    <p>Nullam id dolor</p>
-                </div>
-            </li>
-            <li class="span2">
-                <div class="thumbnail">
-                    <a href="perfil.shtml"><img src="/assets/img/rl/avatar-170.jpg" alt="Rodrigo Medeiros"></a>
-                    <strong>Rodrigo Medeiros</strong>
-                    <p>Nullam id dolor</p>
-                </div>
-            </li>
-            <li class="span2">
-                <div class="thumbnail">
-                    <a href="perfil.shtml"><img src="/assets/img/rl/avatar-170.jpg" alt="Rodrigo Medeiros"></a>
-                    <strong>Rodrigo Medeiros</strong>
-                    <p>Nullam id dolor</p>
-                </div>
-            </li>
-            <li class="span2">
-                <div class="thumbnail">
-                    <a href="perfil.shtml"><img src="/assets/img/rl/avatar-170.jpg" alt="Rodrigo Medeiros"></a>
-                    <strong>Rodrigo Medeiros</strong>
-                    <p>Nullam id dolor</p>
-                </div>
-            </li>
-            <li class="span2">
-                <div class="thumbnail">
-                    <a href="perfil.shtml"><img src="/assets/img/rl/avatar-170.jpg" alt="Rodrigo Medeiros"></a>
-                    <strong>Rodrigo Medeiros</strong>
-                    <p>Nullam id dolor</p>
-                </div>
-            </li>
-            <li class="span2">
-                <div class="thumbnail">
-                    <a href="perfil.shtml"><img src="/assets/img/rl/avatar-170.jpg" alt="Rodrigo Medeiros"></a>
-                    <strong>Rodrigo Medeiros</strong>
-                    <p>Nullam id dolor</p>
-                </div>
-            </li>
-        </ul>
 
     </div>
 
--- apps/frontend/modules/institucional/templates/creditosSuccess.php
+++ apps/frontend/modules/institucional/templates/creditosSuccess.php
@@ -3,20 +3,33 @@
 <div class="span4">
 <h6>Institucional</h6>
 	<ul class="nav nav-tabs nav-stacked">
-		<li><a href="sobre.shtml">Sobre a Robô Livre</a></li>
+		<li><a href="<?php echo url_for("institucional/sobre") ?>">Sobre a Robô Livre</a></li>
 		<li><a href="<?php echo url_for("institucional/instituicoesParceiras") ?>">Instituições parceiras</a></li>
-		<li><a href="sobre.shtml">Clipping</a></li>
 		<li><a href="<?php echo url_for("institucional/apresentacoes") ?>">Apresentações</a></li>
-		<li class="active"><a href="sobre.shtml">Publicações científicas</a></li>
+		<li class="active"><a href="<?php echo url_for("institucional/publicacoesCientificas") ?>">Publicações científicas</a></li>
 	</ul>
         <a href="<?php echo url_for("perfil/index") ?>" class="btn"><i class="icon-chevron-left icon-gray"></i> Início</a>
 </div>
 
 
-HELLO WORLD!
-minha imagem:
+<div class="span8">
 
+<div class="page-header">
+<h1>Créditos</h1>
+</div>
 
-<img src="<?php echo image_path("/assets/img/rl/_avatar-default-large.png") ?>">
+<h2>Imagens e ícones</h2>
+<ul>
+	<li><a href="http://glyphicons.com/">Glyphicons Free</a></li>
+	<li><a href="http://thenounproject.com/">The Noun Project</a></li>
+</ul>
+
+<h2>Software</h2>
+<ul>
+	<li>Estruturado com <a href="http://twitter.github.com/bootstrap/">Boostrap</a> (do <a href="http://twitter.com/twitter" target="_blank">@twitter</a> por <a href="http://twitter.com/mdo">@mdo</a> e <a href="http://twitter.com/fat">@fat</a>)</li>
+	<li>Construído com <a href="http://www.symfony-project.org/">Symfony</a></li>
+</ul>
+
+</div>
 
 </div><!-- /row -->
