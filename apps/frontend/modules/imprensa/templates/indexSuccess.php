4ee26d84f86121dd750a7a96d72e4c4357f674e0
Rodrigo Muniz <eu@rodrigomuniz.com>
2012-07-18 16:43:22 -0300

    Página "Ná mídia" com conteúdo final para lançamento
    Clipping com lista de links verdadeira

--- apps/frontend/modules/imprensa/templates/indexSuccess.php
+++ apps/frontend/modules/imprensa/templates/indexSuccess.php
@@ -79,17 +79,55 @@
             Robótica como instrumento educacional</a></li>
         </ul>
 
-        <h3>Televisão ∙ <small>Vídeos de matérias exibidas na TV</small></h3>
+        <h3>Televisão ∙ <small>Vídeos de matérias exibidas na TV e online</small></h3>
         <ul class="nav nav-tabs nav-stacked">
-          <li><a href="#" rel="external"><span class="label label-success pull-right">
-            Jornal do Commercio ∙ 20.05.2012 <i class="icon-facetime-video icon-white"></i></span> 
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula</a></li>
+
+          <li><a href="http://g1.globo.com/videos/pernambuco/bom-dia-pe/t/edicoes/v/expoideia-mostra-invencoes-que-usa-tecnologia-reaproveitando-o-que-parece-lixo/1939167" rel="external"><span class="label label-success pull-right">
+            Rede Globo ∙ 09.05.2012 <i class="icon-facetime-video icon-white"></i></span> 
+            Expoideia mostra invenções que usa tecnologia reaproveitando o que parece lixo</a></li>
+
+          <li><a href="http://www.youtube.com/watch?v=wlUtndAG-iI" rel="external"><span class="label label-success pull-right">
+            TV Jornal∙ 09.05.2012 <i class="icon-facetime-video icon-white"></i></span> 
+            Expoidea traz palestras e oficinas sobre tecnologia</a></li>  
+
+          <li><a href="http://www.iteia.org.br/videos/minuto-expoidea-entrevista-com-henrique-foresti/" rel="external"><span class="label label-success pull-right">
+            Iteia.org ∙ 07.05.2012 <i class="icon-facetime-video icon-white"></i></span> 
+            Minuto Expoidea entrevista Henrique Foresti</a></li>
+
+          <li><a href="http://g1.globo.com/jornal-da-globo/noticia/2012/03/bairro-do-recife-abriga-escritorio-de-novas-empresas-e-polo-de-tecnologia.html" rel="external"><span class="label label-success pull-right">
+            TV Globo ∙ 20.03.2012 <i class="icon-facetime-video icon-white"></i></span> 
+            Bairro do Recife abriga escritório de novas empresas e polo de tecnologia</a></li>
+
+          <li><a href="http://youtu.be/YtgdLOdE6Ew" rel="external"><span class="label label-success pull-right">
+            Rede TV ∙ 25.07.2011 <i class="icon-facetime-video icon-white"></i></span> 
+            A robótica é incluída na grade Curricular de Escolas do Recife</a></li>
+
+          <li><a href="http://vimeo.com/17096538" rel="external"><span class="label label-success pull-right">
+            Expoidea 2010 ∙ 01.05.2011 <i class="icon-facetime-video icon-white"></i></span> 
+            Oficina: Robótica para iniciantes</a></li>
 
         </ul>
 
         <h3>Online ∙ <small>Link externo de portais e blogs</small></h3>
         <ul class="nav nav-tabs nav-stacked">
-          <li><a href="#" rel="external"><span class="label label-success pull-right">Portal G1 ∙ 20.05.2012 <i class="icon-share-alt icon-white"></i></span> Duis mollis, est non commodo luctus, nisi erat porttitor ligula</a></li>
+          <li><a href="http://blogs.ne10.uol.com.br/mundobit/2012/07/18/plataforma-de-robotica-ganha-reformulacao-na-campus-party-recife/" rel="external"><span class="label label-success pull-right">Mundo Bit ∙ 18.07.2012 <i class="icon-share-alt icon-white"></i></span>Plataforma de robótica ganha reformulação na Campus Party Recife</a></li>
+          <li><a href="http://www.leiaja.com/tecnologia/2012/aprender-robotica-e-util-para-criancas-e-adolescentes" rel="external"><span class="label label-success pull-right">Leia Já ∙ 30.06.2012 <i class="icon-share-alt icon-white"></i></span>Aprender Robótica é útil para crianças e adolescentes</a></li>
+          <li><a href="http://tvjornal.ne10.uol.com.br/noticia.php?id=4786" rel="external"><span class="label label-success pull-right">TV Jornal ∙ 09.05.2012 <i class="icon-share-alt icon-white"></i></span>Expoidea traz palestras e oficinas sobre tecnologia</a></li>
+          <li><a href="http://www.folhape.com.br/cms/opencms/folhape/pt/edicaoimpressa/arquivos/2012/Maio/09_05_2012/0026.html" rel="external"><span class="label label-success pull-right">Folha PE ∙ 09.05.2012 <i class="icon-share-alt icon-white"></i></span>Feira estimula a inovação</a></li>
+          <li><a href="http://www.old.pernambuco.com/ultimas/nota.asp?materia=20120507100307&assunto=196&onde=Ultimas" rel="external"><span class="label label-success pull-right">Diário de Pernambuco ∙ 05.05.2012 <i class="icon-share-alt icon-white"></i></span>Expoidea com programação voltada para inovação, tecnologia e sustentabilidade</a></li>
+          <li><a href="http://www.folhape.com.br/cms/opencms/folhape/pt/tecnologia/noticias/arquivos/2011/outubro/0145.html" rel="external"><span class="label label-success pull-right">Folha PE ∙ 08.05.2012 <i class="icon-share-alt icon-white"></i></span>Expoidea chega ao Recife apostando na tecnologia e sustentabilidade</a></li>
+          <li><a href="http://expoidea.com.br/2012/blog/robo-livre-faz-exposicao-de-robotica-na-expoidea/" rel="external"><span class="label label-success pull-right">Expoidea 2012∙ 17.04.2012 <i class="icon-share-alt icon-white"></i></span>Robô Livre faz exposição de Robótica na Expoidea</a></li>
+          <li><a href="http://www.blogdomadeira.com.br/educacao/cientista-varginhense-apresenta-projeto-de-robotica-no-catanduvas/" rel="external"><span class="label label-success pull-right">Blog do Madeira ∙ 15.05.2012 <i class="icon-share-alt icon-white"></i></span>Cientista varginhense apresenta projeto de robótica no Catanduvas</a></li>
+          <li><a href="http://www2.cin.ufpe.br/site/lerNoticia.php?s=6&c=71&id=516" rel="external"><span class="label label-success pull-right">Expoidea 2012∙ 17.04.2012 <i class="icon-share-alt icon-white"></i></span>DA do CIn promove palestra sobre Robótica Livre nessa quarta-feira</a></li>
+          <li><a href="http://www.leiaja.com/tecnologia/2012/robotica-livre-e-foco-de-palestra-no-cin" rel="external"><span class="label label-success pull-right">Leia já ∙ 10.04.2012 <i class="icon-share-alt icon-white"></i></span>Robótica livre é foco de palestra no CIn</a></li>
+          <li><a href="http://www.dpnet.com.br/hotsite/2012/nossa_ti/academia-e-mercado.shtml" rel="external"><span class="label label-success pull-right">Diário de Pernambuco ∙ 15.05.2012 <i class="icon-share-alt icon-white"></i></span>O velho debate: a academia deve ou não render-se ao mercado?</a></li>
+          <li><a href="http://www.leiaja.com/tecnologia/2012/robotica-colaborativa-e-descomplicada" rel="external"><span class="label label-success pull-right">Leia Já ∙ 11.04.2012 <i class="icon-share-alt icon-white"></i></span>A robótica colaborativa e descomplicada</a></li>
+          <li><a href="http://www.leiaja.com/tecnologia/2012/robotica-livre-e-foco-de-palestra-no-cin" rel="external"><span class="label label-success pull-right">Leia Já ∙ 10.04.2012 <i class="icon-share-alt icon-white"></i></span>Robótica livre é foco de palestra no CIn</a></li>
+          <li><a href="http://atalho.cesar.org.br/index.php/robos-livres-educacao-e-desmistificacao-da-tecnologia/" rel="external"><span class="label label-success pull-right">Blog Atalho - C.E.S.A.R ∙ 27.12.2011 <i class="icon-share-alt icon-white"></i></span>Robôs livres, Educação e Desmistificação da Tecnologia</a></li>
+          <li><a href="http://siep.ifpe.edu.br/anderson/blog/?p=1367" rel="external"><span class="label label-success pull-right">Blog do Anderson Moreira ∙ 17.04.2011 <i class="icon-share-alt icon-white"></i></span>Feira de Tecnologia do IFPE</a></li>
+          <li><a href="http://www.agenciasebrae.com.br/noticia.kmf?canal=36&cod=10356299&indice=10" rel="external"><span class="label label-success pull-right">Folha PE∙ 14.07.2010<i class="icon-share-alt icon-white"></i></span>Robótica como instrumento educacional</a></li>
+          <li><a href="http://www.uai.com.br/htmls/app/noticia173/2010/09/02/noticia_tecnologia,i=177694/index.shtml" rel="external"><span class="label label-success pull-right">UAI ∙ 02.09.2010 <i class="icon-share-alt icon-white"></i></span>Robô mineirim para todos</a></li>
+          <li><a href="http://www.uai.com.br/htmls/app/noticia173/2010/08/31/noticia_tecnologia,i=177274/index.shtml" rel="external"><span class="label label-success pull-right">UAI ∙ 30.08.2010 <i class="icon-share-alt icon-white"></i></span>Festival de Arte Digital reúne criatividade e tecnologia em BH</a></li>
         </ul>
 
 <hr>
